4bd4edc9e2a188c5ae15b870dec145ba882ac4f4
Konstantin S. <havendv@gmail.com>
2026-04-18 06:08:42 +0400

    feat: Updated OpenAPI spec (#207)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AssemblyAI/Generated/AssemblyAI.Models.TranscriptOptionalParamsSpeakerOptions.g.cs
+++ src/libs/AssemblyAI/Generated/AssemblyAI.Models.TranscriptOptionalParamsSpeakerOptions.g.cs
@@ -4,12 +4,12 @@
 namespace AssemblyAI
 {
     /// <summary>
-    /// Specify options for [Speaker diarization](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization#set-a-range-of-possible-speakers). Use this to set a range of possible speakers.
+    /// Specify options for [Speaker diarization](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers#set-a-range-of-possible-speakers). Use this to set a range of possible speakers.
     /// </summary>
     public sealed partial class TranscriptOptionalParamsSpeakerOptions
     {
         /// <summary>
-        /// The minimum number of speakers expected in the audio file. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization#set-a-range-of-possible-speakers) for more details.<br/>
+        /// The minimum number of speakers expected in the audio file. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers#set-a-range-of-possible-speakers) for more details.<br/>
         /// Default Value: 1
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("min_speakers_expected")]
@@ -17,7 +17,7 @@ namespace AssemblyAI
 
         /// <summary>
         /// &lt;Warning&gt;Setting this parameter too high may hurt model accuracy&lt;/Warning&gt;<br/>
-        /// The maximum number of speakers expected in the audio file. The default depends on audio duration: no limit for 0-2 minutes, 10 for 2-10 minutes, and 30 for 10+ minutes. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization#set-a-range-of-possible-speakers) for more details.
+        /// The maximum number of speakers expected in the audio file. The default depends on audio duration: no limit for 0-2 minutes, 10 for 2-10 minutes, and 30 for 10+ minutes. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers#set-a-range-of-possible-speakers) for more details.
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("max_speakers_expected")]
         public int? MaxSpeakersExpected { get; set; }
@@ -32,12 +32,12 @@ namespace AssemblyAI
         /// Initializes a new instance of the <see cref="TranscriptOptionalParamsSpeakerOptions" /> class.
         /// </summary>
         /// <param name="minSpeakersExpected">
-        /// The minimum number of speakers expected in the audio file. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization#set-a-range-of-possible-speakers) for more details.<br/>
+        /// The minimum number of speakers expected in the audio file. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers#set-a-range-of-possible-speakers) for more details.<br/>
         /// Default Value: 1
         /// </param>
         /// <param name="maxSpeakersExpected">
         /// &lt;Warning&gt;Setting this parameter too high may hurt model accuracy&lt;/Warning&gt;<br/>
-        /// The maximum number of speakers expected in the audio file. The default depends on audio duration: no limit for 0-2 minutes, 10 for 2-10 minutes, and 30 for 10+ minutes. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization#set-a-range-of-possible-speakers) for more details.
+        /// The maximum number of speakers expected in the audio file. The default depends on audio duration: no limit for 0-2 minutes, 10 for 2-10 minutes, and 30 for 10+ minutes. See [Set a range of possible speakers](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers#set-a-range-of-possible-speakers) for more details.
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
--- src/libs/AssemblyAI/Generated/AssemblyAI.Models.TranscriptWord.g.cs
+++ src/libs/AssemblyAI/Generated/AssemblyAI.Models.TranscriptWord.g.cs
@@ -43,7 +43,7 @@ namespace AssemblyAI
         public string? Channel { get; set; }
 
         /// <summary>
-        /// The speaker of the word if [Speaker Diarization](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization) is enabled, else null
+        /// The speaker of the word if [Speaker Diarization](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers) is enabled, else null
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("speaker")]
         public string? Speaker { get; set; }
@@ -73,7 +73,7 @@ namespace AssemblyAI
         /// The channel of the word. The left and right channels are channels 1 and 2. Additional channels increment the channel number sequentially.
         /// </param>
         /// <param name="speaker">
-        /// The speaker of the word if [Speaker Diarization](https://www.assemblyai.com/docs/pre-recorded-audio/speaker-diarization) is enabled, else null
+        /// The speaker of the word if [Speaker Diarization](https://www.assemblyai.com/docs/pre-recorded-audio/label-speakers) is enabled, else null
         /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
