04e084aa3e7abe351a97c872a6d48db249912834
Konstantin S. <havendv@gmail.com>
2026-08-14 19:48:33 +0400

    feat: Updated OpenAPI spec (#267)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AssemblyAI/Generated/AssemblyAI.TranscriptsClient.g.cs
+++ src/libs/AssemblyAI/Generated/AssemblyAI.TranscriptsClient.g.cs
@@ -33,10 +33,17 @@ namespace AssemblyAI
 
         /// <inheritdoc/>
         public global::AssemblyAI.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::AssemblyAI.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::AssemblyAI.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
