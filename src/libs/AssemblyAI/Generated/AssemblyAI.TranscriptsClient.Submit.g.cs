bd2dd5e16d64e03e6203a86bc8d885fdb48a5768
Konstantin S. <havendv@gmail.com>
2026-09-09 07:01:36 +0400

    feat: Updated OpenAPI spec (#280)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/AssemblyAI/Generated/AssemblyAI.TranscriptsClient.Submit.g.cs
+++ src/libs/AssemblyAI/Generated/AssemblyAI.TranscriptsClient.Submit.g.cs
@@ -44,7 +44,6 @@ namespace AssemblyAI
         /// <summary>
         /// Transcribe audio<br/>
         /// &lt;Note&gt;To use our EU server for transcription, replace `api.assemblyai.com` with `api.eu.assemblyai.com`.&lt;/Note&gt;<br/>
-        /// &lt;Tip&gt;Building a load test or submitting a large batch? [See our guide →](/pre-recorded-audio/guides/bulk-transcription-and-load-tests-at-scale)&lt;/Tip&gt;<br/>
         /// Create a transcript from a media file that is accessible via a URL.
         /// </summary>
         /// <param name="request"></param>
@@ -77,7 +76,6 @@ namespace AssemblyAI
         /// <summary>
         /// Transcribe audio<br/>
         /// &lt;Note&gt;To use our EU server for transcription, replace `api.assemblyai.com` with `api.eu.assemblyai.com`.&lt;/Note&gt;<br/>
-        /// &lt;Tip&gt;Building a load test or submitting a large batch? [See our guide →](/pre-recorded-audio/guides/bulk-transcription-and-load-tests-at-scale)&lt;/Tip&gt;<br/>
         /// Create a transcript from a media file that is accessible via a URL.
         /// </summary>
         /// <param name="request"></param>
@@ -703,7 +701,6 @@ namespace AssemblyAI
         /// <summary>
         /// Transcribe audio<br/>
         /// &lt;Note&gt;To use our EU server for transcription, replace `api.assemblyai.com` with `api.eu.assemblyai.com`.&lt;/Note&gt;<br/>
-        /// &lt;Tip&gt;Building a load test or submitting a large batch? [See our guide →](/pre-recorded-audio/guides/bulk-transcription-and-load-tests-at-scale)&lt;/Tip&gt;<br/>
         /// Create a transcript from a media file that is accessible via a URL.
         /// </summary>
         /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
